726eae2cd47ed6fd933b1beecf8561bfb3da7dfd
philip.ojha@gmail.com <philip.ojha@ven.io>
2013-07-15 22:26:55 -0400

    some changes

--- intrepidApp/intrepidApp/classes/Constants.h
+++ intrepidApp/intrepidApp/classes/Constants.h
@@ -23,12 +23,12 @@
 #define APP_TEXT_COLOR [UIColor colorWithRed: 66.0/255 green: 58.0/255 blue: 56.0/255 alpha: 1.0]
 
 #define APP_BG_COLOR [UIColor colorWithRed: 248.0/255 green: 248.0/255 blue: 248.0/255 alpha: 1.0]
-#define APP_SEARCH_COLOR [UIColor colorWithRed: 233.0/255 green: 241.0/255 blue: 230.0/255 alpha: 1.0]
+#define APP_SEARCH_COLOR [UIColor colorWithRed: 231.0/255 green: 238.0/255 blue: 226.0/255 alpha: 1]
 
 #define APP_TOGGLE_SELECTED [UIColor colorWithRed: 202.0/255 green: 199.0/255 blue: 194.0/255 alpha: 1.0]
 #define APP_TOGGLE_UNSELECTED [UIColor colorWithRed: 160.0/255 green: 156.0/255 blue: 155.0/255 alpha: 1.0]
 
-#define NAVIGATION_BG_COLOR [UIColor colorWithRed:66/255.0f green:58/255.0f blue:66/255.0f alpha:0.8]
+#define NAVIGATION_BG_COLOR [UIColor colorWithRed:66/255.0f green:58/255.0f blue:56/255.0f alpha:1]
 
 //#define NAVIGATION_TEXT_COLOR [UIColor colorWithRed:231/255.0f green:238/255.0f blue:226/255.0f alpha:1.0f]
 //#define NAVIGATION_TEXT_OFF_COLOR [UIColor colorWithRed:189/255.0f green:185/255.0f blue:177/255.0f alpha:1.0f]
