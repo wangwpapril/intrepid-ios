d94881b5ad8391d5c0129568f66408f9d37551ff
Madelaine Page <madelaine.page@mail.utoronto.ca>
2013-08-02 09:47:21 -0600

    repulling nothing to commit

--- intrepidApp/intrepidApp/MyTripsViewController.m
+++ intrepidApp/intrepidApp/MyTripsViewController.m
@@ -33,7 +33,6 @@
 {
     [super viewDidLoad];
     
-    
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@" " style:UIBarButtonItemStylePlain target:nil action:nil];
     
 //    [self cityNames];
