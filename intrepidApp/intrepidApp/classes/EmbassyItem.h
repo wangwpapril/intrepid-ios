8f037db3fbdf0717113c48a8fa77c9cb30d16c2a
Madelaine Page <ianpage@Ians-iMac.local>
2013-08-15 02:28:02 -0600

    reformatted embassy content and added canadian content

--- intrepidApp/intrepidApp/classes/EmbassyItem.h
+++ intrepidApp/intrepidApp/classes/EmbassyItem.h
@@ -14,8 +14,15 @@
 @property (nonatomic, strong) NSString *flag;
 @property (nonatomic, strong) NSString *name;
 @property (nonatomic, strong) NSString *description;
+@property (nonatomic, strong) NSString *address;
+@property (nonatomic, strong) NSString *contact;
+@property (nonatomic, strong) NSString *hours;
+@property (nonatomic, strong) NSString *services;
+@property (nonatomic, strong) NSString *notice;
+@property (nonatomic, strong) NSString *assistance;
+@property (nonatomic, strong) NSString *immigration;
 
-+(id)embassyItemOfContinent:(NSString *)continent name:(NSString *)name description:(NSString *)description flag:(NSString *)flag;
++(id)embassyItemOfContinent:(NSString *)continent name:(NSString *)name description:(NSString *)description flag:(NSString *)flag address:(NSString *)address contact:(NSString *)contact hours:(NSString *)hours services:(NSString*)services notice:(NSString *)notice assistance:(NSString*)assistance immigration:(NSString*)immigration;
 
 
 @end
--- intrepidApp/intrepidApp/classes/EmbassyItem.m
+++ intrepidApp/intrepidApp/classes/EmbassyItem.m
@@ -13,13 +13,28 @@
 @synthesize flag;
 @synthesize name;
 @synthesize description;
+@synthesize address;
+@synthesize hours;
+@synthesize contact;
+@synthesize services;
+@synthesize notice;
+@synthesize assistance;
+@synthesize immigration;
 
-+(id)embassyItemOfContinent:(NSString *)continent name:(NSString *)name description:(NSString *)description flag:(NSString *)flag;
++(id)embassyItemOfContinent:(NSString *)continent name:(NSString *)name description:(NSString *)description flag:(NSString *)flag address:(NSString *)address contact:(NSString *)contact hours:(NSString *)hours services:(NSString*)services notice:(NSString *)notice assistance:(NSString*)assistance immigration:(NSString*)immigration;
 {
     EmbassyItem *newEmbassyItem = [[self alloc] init];
     newEmbassyItem.name = name;
     newEmbassyItem.description = description;
     newEmbassyItem.flag = flag;
+    newEmbassyItem.address = address;
+    newEmbassyItem.contact = hours;
+    newEmbassyItem.contact = contact;
+    newEmbassyItem.services = services;
+    newEmbassyItem.notice = notice;
+    newEmbassyItem.assistance = assistance;
+    newEmbassyItem.immigration = immigration;
+    
     return newEmbassyItem;
 }
 
