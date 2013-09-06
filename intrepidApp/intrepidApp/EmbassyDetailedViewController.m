34067ca1b9b4fe2750123b33de91df3c0bbfd73d
Madelaine Page <madelaine@Madelaines-MacBook-Pro.local>
2013-09-06 18:46:32 -0400

    pulling changes

--- intrepidApp/intrepidApp/EmbassyDetailedViewController.m
+++ intrepidApp/intrepidApp/EmbassyDetailedViewController.m
@@ -65,13 +65,13 @@
 - (void)addContent {
     
     //ADDRESS
-    UILabel *addressLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 5, 300, 25)];
+    UILabel *addressLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 5, 300, 30)];
     addressLabel.font = [UIFont fontWithName:@"ProximaNova-Semibold" size:16];
     addressLabel.backgroundColor = [UIColor clearColor];
     addressLabel.text = @"Address";
     
     CGSize size = [embassyItem.address sizeWithFont:[UIFont fontWithName:@"ProximaNova-Light" size:13] constrainedToSize:CGSizeMake(280, 15000) lineBreakMode:NSLineBreakByWordWrapping];
-    UILabel *addressText = [[UILabel alloc] initWithFrame:CGRectMake(20, 35,  280, size.height)];
+    UILabel *addressText = [[UILabel alloc] initWithFrame:CGRectMake(20, 30,  280, size.height + 30)];
     addressText.font = [UIFont fontWithName:@"ProximaNova-Light" size:15];
     addressText.backgroundColor = [UIColor clearColor];
     addressText.textColor = [UIColor blackColor];
@@ -80,8 +80,8 @@
     addressText.numberOfLines = 0;
     
     //TELEPHONE/FAX/EMAIL
-    
-    UILabel *contactLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, addressText.frame.origin.y + size.height + 5, 300, 25)];
+
+    UILabel *contactLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, addressText.frame.origin.y + size.height + 30, 300, 30)];
     contactLabel.font = [UIFont fontWithName:@"ProximaNova-Semibold" size:16];
     contactLabel.backgroundColor = [UIColor clearColor];
     contactLabel.text = @"Telephone/Fax/Email";
@@ -90,7 +90,7 @@
                                embassyItem.fax, embassyItem.email];
     
     CGSize contactSize = [contactLength sizeWithFont:[UIFont fontWithName:@"ProximaNova-Light" size:13] constrainedToSize:CGSizeMake(280, 15000) lineBreakMode:NSLineBreakByWordWrapping];
-    UILabel *contactText = [[UILabel alloc] initWithFrame:CGRectMake(20, contactLabel.frame.origin.y + 35,  280, contactSize.height)];
+    UILabel *contactText = [[UILabel alloc] initWithFrame:CGRectMake(20, contactLabel.frame.origin.y + 30,  280, contactSize.height + 30)];
     contactText.font = [UIFont fontWithName:@"ProximaNova-Light" size:15];
     contactText.backgroundColor = [UIColor clearColor];
     contactText.textColor = [UIColor blackColor];
@@ -100,13 +100,13 @@
     
     //HOURS OF OPERATION
     
-    UILabel *hoursLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, contactText.frame.origin.y + contactSize.height + 5, 300, 25)];
+    UILabel *hoursLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, contactText.frame.origin.y + contactSize.height + 30, 300, 30)];
     hoursLabel.font = [UIFont fontWithName:@"ProximaNova-Semibold" size:16];
     hoursLabel.backgroundColor = [UIColor clearColor];
     hoursLabel.text = @"Hours of Operation";
     
     CGSize hoursSize = [embassyItem.hours sizeWithFont:[UIFont fontWithName:@"ProximaNova-Light" size:13] constrainedToSize:CGSizeMake(280, 15000) lineBreakMode:NSLineBreakByWordWrapping];
-    UILabel *hoursText = [[UILabel alloc] initWithFrame:CGRectMake(20, hoursLabel.frame.origin.y +35,  280, hoursSize.height)];
+    UILabel *hoursText = [[UILabel alloc] initWithFrame:CGRectMake(20, hoursLabel.frame.origin.y + 30,  280, hoursSize.height + 30)];
     hoursText.font = [UIFont fontWithName:@"ProximaNova-Light" size:15];
     hoursText.backgroundColor = [UIColor clearColor];
     hoursText.textColor = [UIColor blackColor];
@@ -116,14 +116,14 @@
     
     //SERVICES OFFERED
     
-    UILabel *servicesLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, hoursText.frame.origin.y + hoursSize.height + 5, 300, 25)];
+    UILabel *servicesLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, hoursText.frame.origin.y + hoursSize.height + 30, 300, 30)];
     servicesLabel.font = [UIFont fontWithName:@"ProximaNova-Semibold" size:16];
     servicesLabel.backgroundColor = [UIColor clearColor];
     servicesLabel.text = @"Services Offered";
     
     //this needs to be fixed once content is fixed
     CGSize servicesSize = [embassyItem.services sizeWithFont:[UIFont fontWithName:@"ProximaNova-Light" size:13] constrainedToSize:CGSizeMake(280, 15000) lineBreakMode:NSLineBreakByWordWrapping];
-    UILabel *servicesText = [[UILabel alloc] initWithFrame:CGRectMake(20, servicesLabel.frame.origin.y + 35,  280, servicesSize.height)];
+    UILabel *servicesText = [[UILabel alloc] initWithFrame:CGRectMake(20, servicesLabel.frame.origin.y - 60,  280, servicesSize.height + 250)];
     servicesText.font = [UIFont fontWithName:@"ProximaNova-Light" size:15];
     servicesText.backgroundColor = [UIColor clearColor];
     servicesText.textColor = [UIColor blackColor];
@@ -133,13 +133,13 @@
     
     //NOTES
     
-    UILabel *notesLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, servicesText.frame.origin.y + servicesSize.height + 5, 300, 25)];
+    UILabel *notesLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, servicesText.frame.origin.y + servicesSize.height + 160, 300, 30)];
     notesLabel.font = [UIFont fontWithName:@"ProximaNova-Semibold" size:16];
     notesLabel.backgroundColor = [UIColor clearColor];
     notesLabel.text = @"Notes";
     
     CGSize notesSize = [embassyItem.notes sizeWithFont:[UIFont fontWithName:@"ProximaNova-Light" size:14] constrainedToSize:CGSizeMake(280, 15000) lineBreakMode:NSLineBreakByWordWrapping];
-    UILabel *notesText = [[UILabel alloc] initWithFrame:CGRectMake(20, notesLabel.frame.origin.y + 35,  280, notesSize.height + 10)];
+    UILabel *notesText = [[UILabel alloc] initWithFrame:CGRectMake(20, notesLabel.frame.origin.y + 20,  280, notesSize.height + 30)];
     notesText.font = [UIFont fontWithName:@"ProximaNova-Light" size:15];
     notesText.backgroundColor = [UIColor clearColor];
     notesText.textColor = [UIColor blackColor];
