4adc43f27f05366b21d96d79fd3cca7f4e58311a
Madelaine Page <madelaine.page@mail.utoronto.ca>
2014-12-14 18:03:39 -0500

    embassy stuff in progress

--- intrepidApp/intrepidApp/RequestBuilder.m
+++ intrepidApp/intrepidApp/RequestBuilder.m
@@ -25,6 +25,7 @@ static NSString * baseURL = @"https://staging.intrepid247.com/v1/";
     
     [manager GET:requestURL parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         for (NSDictionary *cityDict in responseObject[@"destinations"]) {
+            
             //CityEntity
             NSString *cityName, *cityImage, *continent, *cultureText, *cultureImage;
             NSString *generalText, *generalImage, *localText, *localImage;
@@ -35,7 +36,7 @@ static NSString * baseURL = @"https://staging.intrepid247.com/v1/";
             NSString *phone, *fax, *email, *hours, *notes, *services, *address, *country, *flag;
             
             cityName = cityDict[@"name"];
-            NSLog(@"city name: %@", cityName);
+            //NSLog(@"city name: %@", cityName);
             
             NSDictionary *contentDict = cityDict[@"content"];
             
@@ -62,11 +63,25 @@ static NSString * baseURL = @"https://staging.intrepid247.com/v1/";
             dollarRatio = 6.0;
             
             //This doesn't exist yet, set up when ready
-            //NSDictionary *embassyDict = cityDict[@""];
+            NSArray *embassyArray = cityDict[@"diplomatic_offices"];
+            for (NSDictionary *embassyDict in embassyArray) {
+                phone = embassyDict[@"content.telephone"];
+                fax = embassyDict[@"content.fax"];
+                email = embassyDict[@"content.email"];
+                hours = embassyDict[@"content.hours_of_operation"];
+                notes = embassyDict[@"notes"];
+                services = embassyDict[@"services"];
+                address = embassyDict[@"content.address"];
+                country = embassyDict[@"country"];
+                flag = embassyDict[@"images.other_image"];
+            }
+
             
             
             [[TripManager getInstance] createTripWithCityImage:cityImage withCityName:cityName withContinent:continent withCultureText:cultureText withCultureImage:cultureImage withGeneralText:generalText withGeneralImage:generalImage withLocalImage:localImage withLocalText:localText withSafetyImage:safetyImage withSafetytext:safetyText withClinicsURL:clinicsURL withAlertsURL:alertsURL withWeatherURL:weatherURL withCADToNative:dollarRatio];
-        
+            
+//            [[TripManager getInstance] createEmbassyWithCity:(CityEntity *)cityName withPhone:phone withFax:fax withEmail:email withHours:hours withNotes:notes withServices:services withAddress:address withCountry:country withFlag:flag];
+
         }
     
         
