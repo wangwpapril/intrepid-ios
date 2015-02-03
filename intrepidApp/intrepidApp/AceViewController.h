6e445d5a1215a833a7f5e742fc06b818258821b5
cshah3 <shah.control@gmail.com>
2015-02-03 11:38:06 -0500

    added ace view

---
+++ intrepidApp/intrepidApp/AceViewController.h
@@ -0,0 +1,16 @@
+//
+//  AceViewController.h
+//  intrepidApp
+//
+//  Created by Chaitanya Shah on 2015-02-03.
+//  Copyright (c) 2015 JonCarrHarris Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MenuController.h"
+
+@interface AceViewController : UIViewController
+
+@property (nonatomic, strong) MenuController *mController;
+
+@end
