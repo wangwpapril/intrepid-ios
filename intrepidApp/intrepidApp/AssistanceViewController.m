2ed965e99113008f6ebe7c4f4692236acc04bc2d
jcarrharris <joncarrharris@gmail.com>
2015-03-25 17:27:10 -0400

    finishing up branding

--- intrepidApp/intrepidApp/AssistanceViewController.m
+++ intrepidApp/intrepidApp/AssistanceViewController.m
@@ -104,33 +104,35 @@
 
 - (void)callAssistance
 {
-    NSArray *providers = [TripManager getInstance].currentUser[@"user"][@"company"][@"assistance_providers"];
-    if (providers.count > 0) {
-        UIActionSheet *assitanceSheet = [[UIActionSheet alloc] init];
-        assitanceSheet.title = @"Call";
-        assitanceSheet.delegate = self;
-        for (id assistance in providers) {
-            NSString *formattedNum = [assistance[@"phone"] stringByReplacingOccurrencesOfString:@" " withString:@""];
-            [assitanceSheet addButtonWithTitle:[NSString stringWithFormat:@"%@ (%@)", assistance[@"name"], formattedNum]];
-        }
-        assitanceSheet.cancelButtonIndex = [assitanceSheet addButtonWithTitle:@"Cancel"];
-        [assitanceSheet showInView:self.view];
-    } else {
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry"
-                                                            message:@"There are no ACE Assistance providers available to help you."
-                                                           delegate:nil
-                                                  cancelButtonTitle:@"OK"
-                                                  otherButtonTitles:nil];
-        [alertView show];
-    }
+//    NSArray *providers = [TripManager getInstance].currentUser[@"user"][@"company"][@"assistance_providers"];
+//    if (providers.count > 0) {
+//        UIActionSheet *assitanceSheet = [[UIActionSheet alloc] init];
+//        assitanceSheet.title = @"Call";
+//        assitanceSheet.delegate = self;
+//        for (id assistance in providers) {
+//            NSString *formattedNum = [assistance[@"phone"] stringByReplacingOccurrencesOfString:@" " withString:@""];
+//            [assitanceSheet addButtonWithTitle:[NSString stringWithFormat:@"%@ (%@)", assistance[@"name"], formattedNum]];
+//        }
+//        assitanceSheet.cancelButtonIndex = [assitanceSheet addButtonWithTitle:@"Cancel"];
+//        [assitanceSheet showInView:self.view];
+//    } else {
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry"
+//                                                            message:@"There are no ACE Assistance providers available to help you."
+//                                                           delegate:nil
+//                                                  cancelButtonTitle:@"OK"
+//                                                  otherButtonTitles:nil];
+//        [alertView show];
+//    }
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel://14166463107"]]];
+
 }
 
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if (buttonIndex != actionSheet.cancelButtonIndex) {
-        NSArray *providers = [TripManager getInstance].currentUser[@"user"][@"company"][@"assistance_providers"];
-        NSString *formattedNum = [providers[buttonIndex][@"phone"] stringByReplacingOccurrencesOfString:@" " withString:@""];
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel://%@", formattedNum]]];
-    }
-}
+//- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+//    if (buttonIndex != actionSheet.cancelButtonIndex) {
+//        NSArray *providers = [TripManager getInstance].currentUser[@"user"][@"company"][@"assistance_providers"];
+//        NSString *formattedNum = [providers[buttonIndex][@"phone"] stringByReplacingOccurrencesOfString:@" " withString:@""];
+//        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"tel://%@", formattedNum]]];
+//    }
+//}
 
 @end
