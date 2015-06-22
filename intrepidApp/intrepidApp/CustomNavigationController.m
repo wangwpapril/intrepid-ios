be5b46cebdd1945b1674059b78ef71fad8165121
jcarrharris <joncarrharris@gmail.com>
2015-06-22 16:05:26 -0400

    added branch

--- intrepidApp/intrepidApp/CustomNavigationController.m
+++ intrepidApp/intrepidApp/CustomNavigationController.m
@@ -41,7 +41,7 @@
 }
 
 - (void)callAssistance {
-    if ([self.viewControllers.lastObject isKindOfClass:[TripsViewController class]]) {
+    if ([self.viewControllers.lastObject isKindOfClass:[TripsViewController class]]) {®
         AssistanceViewController *viewController = [self.storyboard instantiateViewControllerWithIdentifier:@"assistance"];
         viewController.tripsAssistance = true;
         [self pushViewController:viewController animated:YES];
